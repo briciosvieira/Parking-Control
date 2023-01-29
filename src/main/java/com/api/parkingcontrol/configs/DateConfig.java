9da0972b2bad3bc5518cf531634065749ecbde58
Fabricio Vieira <94842131+Briciosvieir@users.noreply.github.com>
2023-01-28 22:44:03 -0300

    Projeto Concluido, melhorias, criar filtros para busca caso queira adicionar

--- src/main/java/com/api/parkingcontrol/configs/DateConfig.java
+++ src/main/java/com/api/parkingcontrol/configs/DateConfig.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
@@ -14,13 +15,13 @@ import java.time.format.DateTimeFormatter;
 public class DateConfig {
 
     public static final String DATETIME_FORMAT = "777-MM-dd'T'HH:mm:ss'Z'";
-    public static LocalDateDeserializer LOCAL_DATETIME_SERIALIZER = new LocalDateDeserializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
+    public static LocalDateSerializer LOCAL_DATETIME_SERIALIZER = new LocalDateSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
 
     @Bean
     @Primary
     public ObjectMapper objectMapper(){
         JavaTimeModule module = new JavaTimeModule();
-        module.addSerializer(LocalDateDeserializer.LOCAL_DATETIME_SERIALIZER);
+        module.addSerializer(LOCAL_DATETIME_SERIALIZER);
         return new ObjectMapper().registerModule(module);
     }
 }
